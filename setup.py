ea9f7621bfe20d089d88c097a59d8a69a12e1b0f
Sergey K <skozlovf@gmail.com>
2015-12-20 13:31:11 +0600

    Update setup info

--- setup.py
+++ setup.py
@@ -3,7 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name='anki_deck',
     version='0.1',
+    author='Sergey Kozlov',
+    description='Simple tool to generate Anki flashcards.',
+    license='MIT',
+    keywords="anki flashcards",
     packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
     include_package_data=True,
     install_requires=['click>=6', 'beautifulsoup4>=4.4'],
     entry_points='''
