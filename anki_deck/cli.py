a3807164d6e868d8d5c6b84304661cc145d0decd
Sergey Kozlov <skozlovf@gmail.com>
2020-01-03 12:31:14 +0600

    Update click

--- anki_deck/cli.py
+++ anki_deck/cli.py
@@ -10,54 +10,47 @@ import logging
 import click
 from anki_deck.parser import get_cards
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
-pass_ctx = click.make_pass_decorator(dict, ensure=True)
 
-
-@click.group(context_settings=CONTEXT_SETTINGS, invoke_without_command=True)
+@click.group(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option('--input-dir', '-i',
               help="Input data dir with 'dict.xdxf' and 'audio/'.")
 @click.option('--dict', '-d',  help='Dictionary file in xdxf format.')
 @click.option('--audio', '-a', help='Directory with audio files in ogg format.')
 @click.option('--words', '-w', help='Input words file.', default='words.txt',
               show_default=True)
-@pass_ctx
+@click.pass_context
 def run(ctx, input_dir, dict, audio, words):
     """Tool to generate cards file which may be imported to Anki."""
 
     logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-    c = click.get_current_context()
-    ctx['input_dir'] = input_dir
-    ctx['dict'] = dict
-    ctx['audio'] = audio
-    ctx['words'] = words
+    ctx.meta['input_dir'] = input_dir
+    ctx.meta['dict'] = dict
+    ctx.meta['audio'] = audio
+    ctx.meta['words'] = words
 
     if input_dir:
         if not dict:
-            ctx['dict'] = op.join(input_dir, 'dict.xdxf')
+            ctx.meta['dict'] = op.join(input_dir, 'dict.xdxf')
         if not audio:
-            ctx['audio'] = op.join(input_dir, 'audio')
-
-    if c.invoked_subcommand is None:
-        c.invoke(txt, None)
+            ctx.meta['audio'] = op.join(input_dir, 'audio')
 
 
 @run.command()
 @click.option('--out', '-o', default='flashcards.txt', show_default=True,
               help="Output filename.")
-@pass_ctx
+@click.pass_context
 def txt(ctx, out):
     """Generate text flashcards file."""
     from anki_deck.flashcards import FlashcardsWriter
     handler = FlashcardsWriter(out)
-    get_cards(ctx['words'], ctx['dict'], ctx['audio'], handler)
+    get_cards(ctx.meta['words'], ctx.meta['dict'], ctx.meta['audio'], handler)
 
 
 @run.command()
 @click.option('--deck-name', '-n', help="Deck name.")
 @click.argument('out')
-@pass_ctx
+@click.pass_context
 def deck(ctx, deck_name, out):
     """Generate apkg deck."""
     name, ext = op.splitext(out)
@@ -72,8 +65,8 @@ def deck(ctx, deck_name, out):
         deck_name = name
 
     from anki_deck.apkg import Deck
-    handler = Deck(out, ctx['audio'], deck_name)
-    get_cards(ctx['words'], ctx['dict'], ctx['audio'], handler)
+    handler = Deck(out, ctx.meta['audio'], deck_name)
+    get_cards(ctx.meta['words'], ctx.meta['dict'], ctx.meta['audio'], handler)
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         'License :: OSI Approved :: MIT License',
     ],
     include_package_data=True,
-    install_requires=['click>=6', 'beautifulsoup4>=4.4'],
+    install_requires=['click>=7', 'beautifulsoup4>=4.4'],
     entry_points='''
         [console_scripts]
         anki_deck=anki_deck.cli:run
